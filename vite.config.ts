1363f6c273de15b7b07753304bbf6bbf42ba6458
clamz <hell.dam.ned.35@gmail.com>
2022-04-06 23:07:14 +0200

    feat: add vite inspector

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,11 @@ import { fileURLToPath, URL } from "url";
 
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
+import Inspector from "vite-plugin-vue-inspector";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [vue()],
+	plugins: [vue(), Inspector()],
 	resolve: {
 		alias: {
 			"@": fileURLToPath(new URL("./src", import.meta.url)),
